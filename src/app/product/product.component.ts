a9e932f40851977ff995726567b8ea878a289602
kevin-bai <897840029@qq.com>
2018-06-10 16:27:53 +0800

    基本页面样式

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  public products: Product[]
+
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      new Product(1,"第1个商品",15,4,"哈哈哈哈哈哈啊哈和",["aaa1","bbbb1"]),
+      new Product(2,"第2个商品",15,3.5,"哈哈哈哈哈哈啊哈和",["aaa2","bbbb2"]),
+      new Product(3,"第3个商品",15,5,"哈哈哈哈哈哈啊哈和",["aaa3","bbbb3"]),
+      new Product(4,"第4个商品",15,2,"哈哈哈哈哈哈啊哈和",["aaa4","bbbb4"]),
+    ]
   }
 
+}
+
+export class Product{
+  constructor(
+    public id:number,
+    public title:string,
+    public price:number,
+    public rating:number,
+    public desc:string,
+    public categories: string[]
+  ) {
+    
+  }
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
+  @Input() rating:number;
+  public starsList:boolean[];
+  // emptyStars:number[];
+  
   constructor() { }
 
   ngOnInit() {
+    console.log(`rating:${this.rating}`)
+    this.getStarNum();
+  }
+  getStarNum():void{
+    this.starsList=[];
+    for (let index = 0; index <5; index++) {
+      this.starsList.push(this.rating>index ? true:false)
+    }
+    console.log(this.starsList)
   }
 
 }
